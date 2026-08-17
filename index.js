7d9612e94d000dab7b779af12ae3c51145682954
nikhilkhandelwaal <nikhilcoder061@gmail.com>
2026-08-17 12:21:45 +0530

    firstbranch

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ console.log("Third commit");
 console.log("Forth Commit");
 console.log("updated Forth Commit");
 console.log("updated 2ndtime Forth Commit");
+console.log("First branch");
 
 //  git restore -> When you have not commit your code
 //  git revert  -> When we want to move into any perticular commit with new commit hash
